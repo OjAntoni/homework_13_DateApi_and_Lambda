ab1cb4525da60d59704f9ff60395840b3d617b9f
OjAntoni <73666164+OjAntoni@users.noreply.github.com>
2021-05-30 09:30:27 +0300

    Update ConsoleDoer.java
    Comment

--- src/Lamda/ConsoleDoer.java
+++ src/Lamda/ConsoleDoer.java
@@ -1,5 +1,8 @@
 package Lamda;
-
+//решил выбрать статический метод, потому что
+//нету смыслa "привязывать" этот метод к какой-то 
+//определённой переменной типа ConsoleDoer. Ведь у 
+//меня лямбла-выражение передаётся в качестве аргумента метода
 @FunctionalInterface
 public interface ConsoleDoer {
     public String doThing(String arg);
